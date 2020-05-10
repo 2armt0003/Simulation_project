1a6c0e8335bbea6fcecc74c1ba70b5ec29c76094
2armt0003 <https://github.com/2armt0003>
2020-05-10 22:31:22 +0200

    initial

---
+++ src/simulation_project/Info.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simulation_project;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author bebo
+ */
+public class Info {
+    public ArrayList<Integer> demand = new ArrayList<Integer>();
+    public ArrayList<Integer> frequancy = new ArrayList<Integer>();
+    public ArrayList<Integer> probaility = new ArrayList<Integer>();
+    public int numOfInputs;
+    
+    public int  avarageDemand;
+    public int ecpectedDemand;
+
+}
